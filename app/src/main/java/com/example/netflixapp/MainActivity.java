573fa60d138c2108f4107c70f230c4007afec431
prj-07 <21je0691@iitism.ac.in>
2022-06-05 20:25:56 +0530

    netflix part 1

---
+++ app/src/main/java/com/example/netflixapp/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.netflixapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+               switch (item.getItemId()){
+                   case R.id.home:
+
+                       return true;
+                   case R.id.menu:
+                       startActivity(new Intent(getApplicationContext(),Menu.class));
+                       overridePendingTransition(0,0);
+                       return true;
+                   case R.id.downloads:
+                       startActivity(new Intent(getApplicationContext(),Downloads.class));
+                       overridePendingTransition(0,0);
+                       return true;
+
+
+               }
+
+                return false;
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/netflixapp/Menu.java
@@ -0,0 +1,44 @@
+package com.example.netflixapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.menu:
+
+                        return true;
+                    case R.id.downloads:
+                        startActivity(new Intent(getApplicationContext(),Downloads.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+
+                }
+
+                return false;
+            }
+        });
+    }
+}
